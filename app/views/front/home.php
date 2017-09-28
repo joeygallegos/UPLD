46c7e8e7a5a53c029cd4ddad5c2fdc467dd7fd8d
nommykid <nommykid@users.noreply.github.com>
2017-09-28 16:28:58 -0700

    solve issue #2
    fixed url redundancy and modified home

--- app/views/front/home.php
+++ app/views/front/home.php
@@ -3,7 +3,7 @@
 <section id="login">
 	<div class="grid">
 		<div class="grid__col grid__col--3-of-5 grid__col--centered">
-			<form id="login" action="/ajax/login.php" method="post" class="animated fadeIn">
+			<form id="login" action="/ajax/login/" method="post" class="animated fadeIn">
 				<input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
 				<input class="full" name="username" type="text" placeholder="Username">
 				<input class="full" name="password" type="password" placeholder="Password">
--- public/assets/scripts/front.js
+++ public/assets/scripts/front.js
@@ -32,7 +32,7 @@ $loginForm.on('submit', function(event) {
 
 	// Send the array of info off to the server for checks
 	$.ajax({
-		url: '/ajax/login/',
+		url: $(this).attr(‘action’),
 		type: 'POST',
 		dataType: 'json',
 		data: formData,
