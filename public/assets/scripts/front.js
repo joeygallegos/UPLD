d2fe3551832f1717a9983266ae1d2c2c22c0fc65
Joey Gallegos <joeygallegos@users.noreply.github.com>
2017-10-07 12:58:25 -0500

    Better JS comments and cleanup

--- public/assets/scripts/front.js
+++ public/assets/scripts/front.js
@@ -1,59 +1,54 @@
 $loginForm = $('form#login');
-
 $loginForm.on('submit', function(event) {
-
-	// Stop it from submitting the lame way
 	event.preventDefault();
 
-	// Vars
+	// form input
 	$username = $('form#login input[type=text][name=username]').val()
 	$password = $('form#login input[type=password][name=password]').val();
 	
-	// Check for actual input
+	// length checks
 	var bothFilled = $username.length > 0 && $password.length > 0
 	if (!bothFilled) {
 		swal('Field error', 'Login fields can\'t be empty, they must both have values', 'error');
-		return;
+		return false;
 	}
 
-	// Validation check
+	// alphanumerical check
 	var matches = /^[a-zA-Z\-]+$/.test($username);
 	if (!matches) {
 		swal('Field error', 'Username must be alphabetical only and can\'t have spaces', 'error');
-		return;
+		return false;
 	}
 
-	// Bundle data into array
+	// data to array
 	var formData = {
 		'action': 'login',
 		'username': $username.trim(),
-		'password': $password.trim()
+		'password': $password
 	}
 
-	// Send the array of info off to the server for checks
+	// push data to server
 	$.ajax({
 		url: $(this).attr('action'),
-		type: 'POST',
+		type: $(this).attr('method'),
 		dataType: 'json',
 		data: formData,
 		cache: false,
 		success: function(data) {
-			// If we can log them in, do it here
-			if (data.response.code !== 1) {
+			if (!data.response.success) {
 				swal("We couldn't log you in!", data.response.message, "error");
 			}
 			else {
+				// reload once authed
 				window.location.reload(false);
 			}
 		},
 		error: function (request, status, error) {
-			// Make a better error handler right here
 			swal("Something stupid happened", "There was a problem while trying to log you in..", "error");
 		}
 	});
-})
+});
 
 $(document).ready(function() {
 	
-})
-
+});
