9c9cc10daadfa7aa3c4aae665d626438de410813
Joey Gallegos <joey@joeygallegos.com>
2019-01-12 18:07:19 -0600

    Fix Session class

--- app/models/Sessions.php
+++ app/models/Sessions.php
@@ -1,20 +1,12 @@
 <?php
+namespace App\Models;
+use App\Models\User;
 class Sessions {
+
 	public static function getUser() {
 		if (isset($_SESSION['user']) && !(is_null($_SESSION['user']))) {
 			return User::where('id', $_SESSION['user']->id)->first();
 		}
 		return null;
-	}
-
-	public static function isSafe($toCheck = 0) {
-		// Checks
-		if (!is_numeric($toCheck)) return false;
-		if ($toCheck <= 0) return false;
-
-		if (!is_null(self::getUser())) {
-			return self::getUser()->id == $toCheck;
-		}
-		return false;
 	}
 }
